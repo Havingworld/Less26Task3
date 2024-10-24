0a1bef4beba049e64230d2d78375915c426a2bf0
havingworld <havingworld@gmail.com>
2024-10-24 17:28:11 +1000

    Добавьте файлы проекта.

---
+++ Less26Task3.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+  //  std::cout << "\xc9\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbb" << std::endl;
+    for (unsigned char c = 0xB3; c < 0xE0; ++c) {
+        std::cout << (char)c << " ";
+    }
+    
+}
