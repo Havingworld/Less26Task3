8ffb8cfda75a69ce18229a889e48575d2449d5eb
havingworld <havingworld@gmail.com>
2024-10-28 11:41:07 +1000

    star display

--- Less26Task3.cpp
+++ Less26Task3.cpp
@@ -1,31 +1,91 @@
 ﻿#include <iostream>
 
-int main()
-{
-	int nDspLgt{ 0 }, nDspHgt{ 0 };
+class Display {
 
-	std::cout << "Enter display lenght: ";
-	std::cin >> nDspLgt;
-	std::cout << "Enter display haight: ";
-	std::cin >> nDspHgt;
+	private:
+		int nDspLgt{ 0 }, nDspHgt{ 0 }, nWndX{ 0 }, nWndY{ 0 }, nWndLgt{ 0 }, nWndHgt{ 0 };
 
-	std::cout << (char)0xC9;
-	for (int i = 0; i < nDspLgt; i++) {
-		std::cout << (char)0xCD;
+	public:
+	void setDisplay() {
+		std::cout << "Enter display lenght: ";
+		std::cin >> nDspLgt;
+		if (nDspLgt < 1) nDspLgt = { 1 };
+		nDspLgt *= 2;
+		std::cout << "Enter display haight: ";
+		std::cin >> nDspHgt;
+		if (nDspHgt < 1) nDspHgt = { 1 };
 	}
-	std::cout << (char)0xBB << std::endl;
-
-	for (int i = 0; i < nDspHgt; i++) {
-		std::cout << (char)0xBA;
-		for (int j = 0; j < nDspLgt; j++) {
-			std::cout << " ";
+	
+	void getDisplay() {
+		std::cout << (char)0xC9;
+		for (int i = 0; i <= nDspLgt; i++) {
+			std::cout << (char)0xCD;
 		}
-		std::cout << (char)0xBA << std::endl;
+		std::cout << (char)0xBB << std::endl;
+		
+		for (int i = 0; i <= nDspHgt; i++) {
+			std::cout << (char)0xBA;
+			for (int j = 0; j <= nDspLgt; j++) {
+				if (nWndLgt != 0 && nWndHgt != 0 && i >= nWndY && i <= nWndY + nWndHgt
+					&& j >= nWndX && j <= nWndX + nWndLgt) { // отрисовка окна					
+					if (i == nWndY && j == nWndX) std::cout << (char)0xDA;
+					if (i == nWndY && j > nWndX && j < nWndX + nWndLgt) std::cout << (char)0xC4;
+					if (i == nWndY && j == nWndX + nWndLgt) std::cout << (char)0xBF;
+
+					if (i > nWndY && i < nWndY + nWndHgt && j == nWndX) std::cout << (char)0xB3;
+					if (i > nWndY && i < nWndY + nWndHgt && j > nWndX && j < nWndX + nWndLgt) std::cout << " ";
+					if (i > nWndY && i < nWndY + nWndHgt && j == nWndX + nWndLgt) std::cout << (char)0xB3;
+
+					if (i == nWndY + nWndHgt && j == nWndX) std::cout << (char)0xC0;
+					if (i == nWndY + nWndHgt && j > nWndX && j < nWndX + nWndLgt) std::cout << (char)0xC4;
+					if (i == nWndY + nWndHgt && j == nWndX + nWndLgt) std::cout << (char)0xD9;
+				}
+				else {
+					std::cout << " ";
+				}
+			}
+			std::cout << (char)0xBA << std::endl;
+		}
+
+		std::cout << (char)0xC8;
+		for (int i = 0; i <= nDspLgt; i++) {
+			std::cout << (char)0xCD;
+		}
+		std::cout << (char)0xBC << std::endl;
+
 	}
 
-	std::cout << (char)0xC8;
-	for (int i = 0; i < nDspLgt; i++) {
-		std::cout << (char)0xCD;
+	void setWindow() {
+		std::cout << "Enter window X point: ";
+		std::cin >> nWndX;
+		if (nWndX < 0) nWndX = { 0 };
+		if (nWndX > nDspLgt - 1) nWndX = { nDspLgt - 1 };
+		std::cout << "Enter window Y point: ";
+		std::cin >> nWndY;
+		if (nWndY < 0) nWndY = { 0 };
+		if (nWndY > nDspHgt - 1) nWndY = { nDspHgt - 1 };
+		std::cout << "Enter window lenght: ";
+		std::cin >> nWndLgt;
+		if (nWndLgt < 1) nWndLgt = { 1 };
+		nWndLgt *= 2;
+		if ((nWndX + nWndLgt) > nDspLgt) nWndLgt = { nWndLgt - nWndX };
+		std::cout << "Enter window haight: ";
+		std::cin >> nWndHgt;
+		if (nWndHgt < 1) nWndHgt = { 1 };
+		if ((nWndY + nWndHgt) > nDspHgt) nWndHgt = { nWndHgt - nWndY };
 	}
-	std::cout << (char)0xBC << std::endl;
+	
+};
+
+int main() {
+	Display* display1 = new Display;
+
+	display1->setDisplay();
+	display1->getDisplay();
+	display1->setWindow();
+	display1->getDisplay();
+
+	delete display1;
+	display1 = { nullptr };
+	return 0;
 }
