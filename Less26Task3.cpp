ec2dab79060035550ae4415a601bcf49dfb10085
havingworld <havingworld@gmail.com>
2024-10-28 11:41:07 +1000

    star display

--- Less26Task3.cpp
+++ Less26Task3.cpp
@@ -2,9 +2,30 @@
 
 int main()
 {
-  //  std::cout << "\xc9\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xbb" << std::endl;
-    for (unsigned char c = 0xB3; c < 0xE0; ++c) {
-        std::cout << (char)c << " "; 
-    }
-    
+	int nDspLgt{ 0 }, nDspHgt{ 0 };
+
+	std::cout << "Enter display lenght: ";
+	std::cin >> nDspLgt;
+	std::cout << "Enter display haight: ";
+	std::cin >> nDspHgt;
+
+	std::cout << (char)0xC9;
+	for (int i = 0; i < nDspLgt; i++) {
+		std::cout << (char)0xCD;
+	}
+	std::cout << (char)0xBB << std::endl;
+
+	for (int i = 0; i < nDspHgt; i++) {
+		std::cout << (char)0xBA;
+		for (int j = 0; j < nDspLgt; j++) {
+			std::cout << " ";
+		}
+		std::cout << (char)0xBA << std::endl;
+	}
+
+	std::cout << (char)0xC8;
+	for (int i = 0; i < nDspLgt; i++) {
+		std::cout << (char)0xCD;
+	}
+	std::cout << (char)0xBC << std::endl;
 }
